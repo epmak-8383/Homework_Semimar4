85394fe9f469beb1c7509e1916db0f19df020c0a
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-11 23:30:14 +0300

    второе задание

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿int SumOfNumber(int a)
+{
+    int sum = 0;
+    for (int i = a; i > 0;)
+    {
+        sum = sum + a%10;
+        i = a/10;
+        a = a/10;
+    }
+    return sum;
+}
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int c = int.Parse(Console.ReadLine());
+    return c;
+}
+
+int a = GetNumber("введите число: ");
+int s = SumOfNumber(a);
+Console.WriteLine($"полученная сумма цифр равна {s}");
