b04165f67880ec43735330d3d4e0e0824fbd46ee
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-11 22:32:21 +0300

    первое задание

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int Power(int a, int b)
+{
+    int result = Convert.ToInt32(Math.Pow(a, b));
+    return result;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int a = EnterData("Введите число: ");
+int b = EnterData("Введите степень: ");
+
+int result = Power(a, b);
+
+Console.WriteLine($"Число {a} в степени {b} равно {result}");
