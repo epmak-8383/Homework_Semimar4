23fb7961aa64868335a05fc784a9863f77f4179a
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-11 23:45:45 +0300

    третье задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(2147483647);
+    }
+    Console.Write($"[{String.Join(", ", array)}]");
+}
